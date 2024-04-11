9c37d17bf9588bef670e0316fb44ac6c64f1f73a
Justin Sohn <sohnjustin2@gmail.com>
2024-04-11 15:23:33 -0700

    added api key to env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,6 +10,7 @@ import {
   doc,
   setDoc,
 } from "firebase/firestore";
+import { FIREBASE_API_KEY } from "@env";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,7 +18,7 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDIOlj1nEEwxrakzMayAUJtUooSSVIJvh0",
+  apiKey: FIREBASE_API_KEY,
   authDomain: "swipedine-7e5a2.firebaseapp.com",
   databaseURL: "https://swipedine-7e5a2-default-rtdb.firebaseio.com",
   projectId: "swipedine-7e5a2",
