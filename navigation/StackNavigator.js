d2a53ad8307545b149f53feb520a2fa55200d709
Justin Sohn <sohnjustin2@gmail.com>
2024-04-13 04:00:03 -0700

    added bottom navigation bar and refactored navigations

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,48 @@
+import React from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../src/screenRoute/LoginScreen";
+import WelcomeScreen from "../src/screenRoute/WelcomeScreen";
+import SignUpScreen from "../src/screenRoute/SignUpScreen";
+import { HandleReservation } from "../src/screenRoute/TimeScreen";
+import TabNavigation from "./TabNavigation";
+import TimeScreen from "../src/screenRoute/TimeScreen";
+
+const Stack = createStackNavigator();
+
+function StackNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen
+        name="Welcome"
+        title="Welcome"
+        component={WelcomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Login"
+        title="Login"
+        component={LoginScreen}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="SignUp"
+        title="Signup"
+        component={SignUpScreen}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="Time"
+        title="Time"
+        component={TimeScreen}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="Main"
+        component={TabNavigation}
+        options={{ headerShown: true }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default StackNavigator;
--- src/screenRoute/ProfileScreen.jsx
+++ src/screenRoute/ProfileScreen.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import { Text } from "react-native";
 
 function ProfileScreen() {
-  return <div>ProfileScreen</div>;
+  return <Text>ProfileScreen</Text>;
 }
 
 export default ProfileScreen;
