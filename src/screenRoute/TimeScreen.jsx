3e0c8325ebfa9c5220900789c22d2dd136585d32
Henry Nguyen <henryng310@yahoo.com>
2024-05-06 19:35:25 -0700

    More styles adjustments

--- src/screenRoute/TimeScreen.jsx
+++ src/screenRoute/TimeScreen.jsx
@@ -77,7 +77,7 @@ const TimeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    backgroundColor: "#aaf0d1", //mint green
     alignItems: "center",
     justifyContent: "center",
     padding: 20,
